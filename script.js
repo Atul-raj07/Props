87345e320cf4caaf3b866e7087157e72ece439f8
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-01-06 23:39:03 +0530

    taiwind uploaded

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 var body = document.querySelector(".preloader")
-document.querySelector("h1").addEventListener("click",function(){
-    body.style.backgroundColor = "red"
-})
+var h1 = document.querySelector("h1");
 
+var tl = gsap.timeline()
+
+
+
+let i = 0
+ var int = setInterval(function(){
+    if(i<100){
+        i++
+        // console.log(i)
+        h1.innerHTML = i + "%"
+        
+    }else{
+        // h1.innerHTML = ,
+        tl.to("h1",{
+            duration:2,
+            delay : 1,
+            opacity :0
+        }) 
+        tl.to(".preloader",{
+            top: "-100%",
+            // delay:2,
+        }) 
+        
+    }
+}, 40)
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    colors:{
+      primary : '#F4F0EA',
+      secondary : '#4D3D30'
+    },
     extend: {},
   },
   plugins: [],
