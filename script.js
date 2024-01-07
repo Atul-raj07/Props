20c6701c26bc6e173eb5a46fa153c95936fc7ec3
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-01-07 23:40:55 +0530

    navbar and preloader done

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 var body = document.querySelector(".preloader")
 var h1 = document.querySelector("h1");
-
+var pageloader = document.querySelector(".preloader")
 var tl = gsap.timeline()
+var image = document.querySelector("img")
 
-
-
-let i = 0
- var int = setInterval(function(){
-    if(i<100){
+let i = 0;
+// while(i < 100){
+//     i++
+// }
+const intervalId = setInterval(() => {
+    if(i < 100){
         i++
-        // console.log(i)
-        h1.innerHTML = i + "%"
-        
+        console.log(i)
+        h1.innerHTML = i + "%";
     }else{
-        // h1.innerHTML = ,
-        tl.to("h1",{
-            duration:2,
-            delay : 1,
-            opacity :0
-        }) 
-        tl.to(".preloader",{
-            top: "-100%",
-            // delay:2,
-        }) 
-        
+        tl.to(h1,{
+            // y : "10%",
+            delay : 2,
+            innerHTML : "PROPS",
+            ease: "power4.out",
+        })
+        tl.to(image,{
+            // x : 100,
+            opacity : 1,
+            display : "inline",
+            duration : 2,
+            ease: "power4.out",
+        })
+        tl.to(image,{
+            x : 100,
+            // display : "inline",
+            // duration : 5 ,
+            ease: "power4.out",
+            opacity : 0
+        })
+          tl.to(pageloader,{
+            y : "-100%",
+            duration : 1,
+        })
     }
-}, 40)
+}, 30)
