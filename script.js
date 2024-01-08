0da043aef1803469fbde84f1660c8867b1909903
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-01-08 23:35:21 +0530

    page1 completed with all essential animations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var body = document.querySelector(".preloader")
-var h1 = document.querySelector("h1");
+
+var h1 = document.querySelector(".preloader h1");
 var pageloader = document.querySelector(".preloader")
 var tl = gsap.timeline()
 var image = document.querySelector("img")
@@ -16,7 +16,7 @@ const intervalId = setInterval(() => {
     }else{
         tl.to(h1,{
             // y : "10%",
-            delay : 2,
+            delay : 1,
             innerHTML : "PROPS",
             ease: "power4.out",
         })
@@ -27,16 +27,42 @@ const intervalId = setInterval(() => {
             duration : 2,
             ease: "power4.out",
         })
-        tl.to(image,{
-            x : 100,
-            // display : "inline",
-            // duration : 5 ,
-            ease: "power4.out",
-            opacity : 0
-        })
+        // tl.to(image,{
+        //     x : 100,
+        //     // display : "inline",
+        //     // duration : 5 ,
+        //     ease: "power4.out",
+        //     opacity : 0
+        // })
           tl.to(pageloader,{
             y : "-100%",
             duration : 1,
         })
     }
-}, 30)
+}, 20)
+var prdctofday = document.querySelector(".productofday")
+
+gsap.to(".productofday",{
+    y : -600,
+    scrollTrigger:{
+        trigger:".productofday",
+        scroller:"body",
+        markers:true,
+        start : "top 90%",
+        end:"top -50%",
+        scrub:1,
+        // pin:true,
+    }
+})
+gsap.to(".overlay",{
+    y : -600,
+    scrollTrigger:{
+        trigger:".page1",
+        scroller:"body",
+        markers:true,
+        start : "top 0%",
+        end:"top 0%",
+        scrub:1,
+        // pin:true,
+    }
+})
