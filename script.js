a606b7649f439de0e06ae1150f80461a82056eb8
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-01-18 17:23:52 +0530

    page6 latest prdctarrival created

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
 
 var h1 = document.querySelector(".preloader h1");
 var pageloader = document.querySelector(".preloader")
@@ -46,7 +74,7 @@ gsap.to(".productofday",{
     y : -600,
     scrollTrigger:{
         trigger:".productofday",
-        scroller:"body",
+        scroller:".main",
         // markers:true,
         start : "top 80%",
         end:"top -50%",
@@ -58,7 +86,7 @@ gsap.to(".page2 img",{
     y : -100,
     scrollTrigger:{
         trigger:".page2",
-        scroller:"body",
+        scroller:".main",
         // markers:true,
         start : "top 40%",
         end:"top -60%",
@@ -84,7 +112,7 @@ gsap.to(".overlay",{
     y : -100,
     scrollTrigger:{
         trigger:".page1",
-        scroller:"body",
+        scroller:".main",
         // markers:true,
         start : "top 0%",
         end:"top 0%",
@@ -111,7 +139,7 @@ gsap.to(".product",{
     x: `-${totalWidth}px`,
     scrollTrigger:{
         trigger:".page3",
-        scroller:"body",
+        scroller:".main",
         // markers:true,
         start : "top 60%",
         end:"top -100%",
@@ -131,8 +159,8 @@ gsap.to(".prdctcatg",{
     transform : "translateX(-370%)",
     scrollTrigger:{
         trigger:".page4",
-        scroller:"body",
-        markers:true,
+        scroller:".main",
+        // markers:true,
         start : "top 0%",
         // end:"top -50%",
         scrub:1,
@@ -141,10 +169,22 @@ gsap.to(".prdctcatg",{
 })
 gsap.to(".page5 img",{
 
-    scale : 1.5,
+    scale : 1,
     scrollTrigger:{
         trigger:".page5",
-        scroller:"body",
+        scroller:".main",
+        // markers : true,
+        start:"top 50%",
+        end: "top -100%",
+        // pin : true,
+        scrub :1,
+    }
+})
+gsap.to(".page5",{
+    y : -100,
+    scrollTrigger:{
+        trigger:".page5",
+        scroller:".main",
         markers : true,
         start:"top 50%",
         end: "top -50%",
